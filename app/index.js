446e1ebcff5f48fc65b8014b8ea2a007ff2674fa
MGSK-J <sonalikalpani@outlook.com>
2023-10-06 22:21:17 +0530

    Did few styling in index.js ScrollView

--- app/index.js
+++ app/index.js
@@ -12,13 +12,33 @@ const Home=()=>{
     return(
         <SafeAreaView style={{flex:1,backgroundColor:COLORS.lightWhite
         }}>
-        <View>
           <Stack.Screen
            options={{
             headerStyle:{backgroundColor:COLORS.lightWhite},
+            headerShadowVisible:false,
+            headerLeft:()=>(
+                <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%"/>
+            ),
+            headerRight:()=>(
+                <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%"/>
+            ),
+            headerTitle:""
            }}
           />
-        </View>
+          <ScrollView showsVerticalScrollIndicator={false}>
+           <View
+            style={{
+                flex:1,
+                padding:SIZES.medium
+            }}
+            >
+                <Welcome
+                 
+                />
+                <Popularjobs />
+                <Nearbyjobs />
+           </View>
+          </ScrollView>
         </SafeAreaView>
     )
 }
